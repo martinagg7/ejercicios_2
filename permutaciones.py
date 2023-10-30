9e7940f53f7dd0884193923d7f1623ad9582a6e2
Martina Garcia <martinagg@myuax.com>
2023-10-30 14:50:51 +0100

    .

--- permutaciones.py
+++ permutaciones.py
@@ -1,8 +1,16 @@
 per=[]
-n=int(input("Introduce el número de la permutacion :"))
-
-def permutaciones(n,per):
+def permutaciones(per):
+    max_glob=[]
     for lista in per:
-        if n not in lista:
-            lista_importar=lista
-    permutacion=lista_importar.append(n)
+        max_l=max(lista)
+        max_glob.append(max_l)
+    num_per=max(max_l)
+    for lista in per:
+        if num_per not in lista:
+            cadena=lista
+    permutacion=cadena.append(num_per)
+    return permutacion
+        
+
+
+        
